b5a6148bb7c5cc28f8b86cfb7212c60154b83eaf
DoVietHoang1712 <hoang171220001@gmail.com>
2021-04-30 10:48:34 +0700

    Modify

--- src/modules/user/controller/identify-device.controller.ts
+++ src/modules/user/controller/identify-device.controller.ts
@@ -1,11 +1,12 @@
-import { Body, Controller, HttpStatus, Post, Put } from "@nestjs/common";
+import { SystemRoles } from './../../../common/decorator/auth.decorator';
+import { Body, Controller, Get, HttpStatus, Param, Post, Put } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { ApiErrorResponse } from "../../../common/decorator/api.decorator";
 import { Authorization } from "../../../common/decorator/auth.decorator";
 import { ReqUser } from "../../../common/decorator/user.decorator";
 import { CommonResultResponseDto } from "../../../common/dto/response/common-result-response.dto";
 import { ResponseDto } from "../../../common/dto/response/response.dto";
-import { UserErrorCode } from "../common/user.constant";
+import { SystemRole, UserErrorCode } from "../common/user.constant";
 import { IdentifyDeviceDto } from "../dto/identify-device.dto";
 import { UserDocument } from "../entities/user.entity";
 import { UserService } from "../service/user.service";
@@ -18,23 +19,44 @@ export class IdentifyDeviceController {
         private readonly userService: UserService,
     ) { }
 
+    @Get("/request-change")
+    @SystemRoles(SystemRole.ADMIN)
+    async getRequestChange(): Promise<any> {
+        const data = await this.userService.getRequestChangeIdentity();
+        return ResponseDto.response(data);
+    }
+
+    @Post("/request-change")
+    @SystemRoles(SystemRole.USER)
+    async createRequestChange(@ReqUser() user: UserDocument): Promise<any> {
+        const data = await this.userService.sendRequestChangeIdentity(user);
+        return ResponseDto.response(data);
+    }
+
+    @Post("/request-change/:id")
+    @SystemRoles(SystemRole.ADMIN)
+    async process(@ReqUser() user: UserDocument, @Param("id") id: string): Promise<any> {
+        const data = await this.userService.process(id);
+        return ResponseDto.response(data);
+    }
+
     @Post()
     @ApiErrorResponse(
         HttpStatus.BAD_REQUEST,
         [
-            { 
+            {
                 errorCode: UserErrorCode.BAD_REQUEST_EMPTY_CLIENT_DEVICE_ID,
                 errorDescription: "Không có thông tin client device id khi đăng nhập (Có thể do đăng nhập tử Web)",
             },
-            { 
+            {
                 errorCode: UserErrorCode.BAD_REQUEST_DEVICE_IDENDIFIED,
                 errorDescription: "Người dùng đã được xác nhận thiết bị",
             },
-            { 
+            {
                 errorCode: UserErrorCode.BAD_REQUEST_CLIENT_DEVICE_ID_EXIST,
                 errorDescription: "Thiết bị đã được đăng ký cho một người dùng khác",
             },
-            { 
+            {
                 errorCode: UserErrorCode.BAD_REQUEST_WRONG_PASSWORD,
                 errorDescription: "Mật khẩu không chính xác",
             },
