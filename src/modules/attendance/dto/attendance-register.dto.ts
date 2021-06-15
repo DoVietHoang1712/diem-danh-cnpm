8e08bd878a646c8505a5a59c1e1d6df4d425bf8c
DoVietHoang1712 <hoangdo@DESKTOP-65RRTNT.localdomain>
2021-06-16 00:03:28 +0700

    Modify

--- src/modules/attendance/dto/attendance-register.dto.ts
+++ src/modules/attendance/dto/attendance-register.dto.ts
@@ -2,7 +2,6 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsOptional, IsString } from "class-validator";
 
 export class AttendanceRegisterDto {
-    @IsString()
     @ApiProperty()
     @IsOptional()
     otp?: number;
