ba9edd645f59fbda45f2a79b881c0377fa739f1f
DoVietHoang1712 <hoangdo@DESKTOP-65RRTNT.localdomain>
2021-06-15 22:43:08 +0700

    Modify

--- src/modules/attendance/dto/attendance-register.dto.ts
+++ src/modules/attendance/dto/attendance-register.dto.ts
@@ -1,10 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString } from "class-validator";
+import { IsOptional, IsString } from "class-validator";
 
 export class AttendanceRegisterDto {
     @IsString()
     @ApiProperty()
-    otp: string;
+    @IsOptional()
+    otp?: string;
 
     @IsString()
     @ApiProperty()
