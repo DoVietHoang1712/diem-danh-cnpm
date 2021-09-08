e841edf16164f153195369abbee84a7075faaeb2
DoVietHoang1712 <hoang171220001@gmail.com>
2021-09-08 20:48:29 +0700

    Modify

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 echo 'Starting to Deploy...'
-ssh ubuntu@18.139.117.111 " echo '111'
+ssh ubuntu@54.169.160.52 " echo '111'
         echo '123s'
         "
 echo 'Deployment completed successfully'
