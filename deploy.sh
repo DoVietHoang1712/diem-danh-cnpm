55534cf65b5255f46f7ac7bfb5b3d9654735efec
DoVietHoang1712 <52753161+DoVietHoang1712@users.noreply.github.com>
2021-09-09 16:38:00 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
  
 echo 'Starting to Deploy...'
-ssh ubuntu@54.169.160.52 " echo '111'
-        echo '123s'
+ssh ubuntu@54.169.160.52 " sudo docker image prune -f 
+        cd diem-danh-cnpm 
+        sudo docker-compose down
+        git fetch origin main
+        git reset --hard origin/main  &&  echo 'You are doing well'
+        sudo docker-compose build && sudo docker-compose up -d
         "
 echo 'Deployment completed successfully'
